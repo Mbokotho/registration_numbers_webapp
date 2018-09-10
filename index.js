04a806a155e4327513b56051ad42c6bcf3bd7efc
mbokotho <mplwando@gmail.com>
2018-09-10 12:51:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const app = express();
+
+const exphbs = require('express-handlebars');
+
+const bodyParser = require('body-parser');
+
+const moment = require('moment');
+
+const flash = require('express-flash');
+
+const session = require('express-session');
+
+// const Greeting = require('./Greet');
+
+// const Set = Greeting();
+
+let greetRoutes = require('./routes/greetings');
+
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+// which db connection to use
+const connectionString =
+  process.env.DATABASE_URL ||
+  'postgresql://coder:pg123@localhost:5432/spMynames';
+
+const pool = new Pool({
+    connectionString,
+    ssl: useSSL
+});
+
+app.engine(
+    'handlebars',
+    exphbs({
+        defaultLayout: 'main',
+        helpers: {
+            timestamp: function () {
+                return moment(this.timestamp).fromNow();
+            }
+        }
+    })
+);
+
+app.set('view engine', 'handlebars');
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(
+    session({
+        secret: '<add a secret string here>',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+app.use(flash());
+
+
+let PORT = process.env.PORT || 3020;
+
+app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
+});
